6e8e51b93e31fb0a343a069eecce17c21cd88835
Glitch (hello-webpage) <none>
2020-08-25 13:14:52 +0000

    🚎🗾 Checkpoint ./index.html:15679941/1813 ./main.js:15679941/12118 ./systems.js:15679941/5344 ./components.js:15679941/2619

---
+++ components.js
@@ -0,0 +1,61 @@
+import { TagComponent, Component, Types } from "https://unpkg.com/ecsy@0.4.1?module";
+
+export class Collidable extends TagComponent {}
+export class Collider extends TagComponent {}
+export class Recovering extends TagComponent {}
+export class Moving extends TagComponent {}
+
+export class PulsatingScale extends Component {
+  static get schema() {
+    return {
+      offset: { type: Types.Number, default: 0 },
+    };
+  }
+}
+
+export class Object3D extends Component {
+  static get schema() {
+    return {
+      object: { type: Types.Ref },
+    };
+  }
+}
+
+export class Timeout extends Component {
+  static get schema() {
+    return {
+      timer: { type: Types.Number },
+      addComponents: { type: Types.Array },
+      removeComponents: { type: Types.Array },
+    };
+  }
+}
+
+export class PulsatingColor extends Component {
+  static get schema() {
+    return {
+      offset: { type: Types.Number },
+    };
+  }
+}
+
+PulsatingColor.schema = {
+};
+
+export class Colliding extends Component {
+  static get schema() {
+    return {
+      value: { type: Types.Boolean },
+    };
+  }
+}
+
+export class Rotating extends Component {
+  static get schema() {
+    return {
+      enabled: { type: Types.Boolean },
+      rotatingSpeed: { type: Types.Number },
+      decreasingSpeed: { type: Types.Number, default: 0.001 },
+    };
+  }
+}
